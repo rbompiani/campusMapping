8c7c779703cfc09b8e13893a58e9917287e5c426
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-01-13 23:34:53 -0800

    toggle building visiblity with checkbox

--- OSUjs/toggles.js
+++ OSUjs/toggles.js
@@ -1,20 +1,10 @@
- // functions for Pamoramio Photos
-function showPhotos() {
-  panoramioLayer.setMap(map);
-}
-
-function hidePhotos() {
-  panoramioLayer.setMap(null);
-}
-
-
 // functions for Fusion Tables
 function hideFusion() {
   fusionLayer.setMap(null);
 }
 
 function showFusion() {
-  fusionLayer.setMap(map);
+  map.data.setStyle({ visible: false });
 }
 
 function hideParking() {
@@ -35,30 +25,21 @@ function hideOverlays() {
 }
 
 // functions for Layers
-function testBuilding() {
-	if(document.getElementById('buildings').checked == true)
-  { showFusion(); }
-  else
-  { hideFusion(); }
+function toggleBuildings(state) {
+  map.data.setStyle({ visible: document.getElementById("buildings").checked });
 }
 
 function testPhotos() {
-	if(document.getElementById('photos').checked == true)
-  { showPhotos(); }
-  else
-  { hidePhotos(); }
+  if (document.getElementById('photos').checked == true) { showPhotos(); }
+  else { hidePhotos(); }
 }
 
 function testOverlays() {
-	if(document.getElementById('overlays').checked == true)
-  { showOverlays(); }
-  else
-  { hideOverlays(); }
+  if (document.getElementById('overlays').checked == true) { showOverlays(); }
+  else { hideOverlays(); }
 }
 
 function testParking() {
-	if(document.getElementById('parking').checked == true)
-  { showParking(); }
-  else
-  { hideParking(); }
+  if (document.getElementById('parking').checked == true) { showParking(); }
+  else { hideParking(); }
 }
