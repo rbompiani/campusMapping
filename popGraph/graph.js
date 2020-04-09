c57c02bc84af6b52973ffd5826072d47dd0f698b
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-04-09 15:11:21 -0700

    added svg area path to graph

--- popGraph/graph.js
+++ popGraph/graph.js
@@ -8,8 +8,8 @@ let projectedGsf = [];
 
 // ---- GRAPH CONTAINER SETUP --- //
 const margin = { top: 40, right: 20, bottom: 50, left: 100 };
-const graphWidth = 1000 - margin.left - margin.right;
-graphHeight = 1500 - margin.top - margin.bottom;
+const graphWidth = 800 - margin.left - margin.right;
+graphHeight = 500 - margin.top - margin.bottom;
 
 const svg = d3.select('.canvas')
     .append('svg')
@@ -24,8 +24,8 @@ const graph = svg.append('g')
 
 // ---- GRAPH SCALES / AXES --- //
 const yearScale = d3.scaleLinear().range([0, graphWidth]);
-const popScale = d3.scaleLinear().range([0, graphHeight]);
-const gsfScale = d3.scaleLinear().range([0, graphHeight]);
+const popScale = d3.scaleLinear().range([graphHeight, 0]);
+const gsfScale = d3.scaleLinear().range([graphHeight, 0]);
 
 const yearAxisGroup = graph.append('g')
     .attr('class', 'year-axis')
@@ -39,16 +39,17 @@ const gsfAxisGroup = graph.append('g')
 
 
 // ---- RETRIEVE DATA --- //
-db.collection("population").get().then((res) => {
+db.collection("population").orderBy('year').get().then((res) => {
     res.forEach((doc) => {
         //console.log(`${doc.id} => ${doc.data()}`);
         const document = { ...doc.data(), id: doc.id }
         pop.push(document);
 
     });
+
     // set graph axes domains
     yearScale.domain(d3.extent(pop, p => p.year));
-    popScale.domain([0, d3.max(pop, p => p.students)]);
+    popScale.domain([10000, d3.max(pop, p => p.students)]);
 
     const yearAxis = d3.axisBottom(yearScale)
         .ticks(4)
@@ -59,11 +60,41 @@ db.collection("population").get().then((res) => {
 
     yearAxisGroup.call(yearAxis);
     popAxisGroup.call(popAxis);
+
+    const points = graph.selectAll('circle')
+        .data(pop);
+
+    points.enter()
+        .append('circle')
+        .attr('r', 2)
+        .attr('cx', d => yearScale(d.year))
+        .attr('cy', d => popScale(d.students))
+        .attr('fill', 'orange');
+
+    graph.append("path")
+        .datum(pop)
+        .attr("fill", "orange")
+        .attr("fill-opacity", .3)
+        .attr("stroke", "orange")
+        .attr("d", d3.area()
+            .x(function (d) { return yearScale(d.year) })
+            .y0(graphHeight)
+            .y1(function (d) { return popScale(d.students) })
+        )
+    /* draw path
+    graph.append("path")
+        .datum(data)
+        .attr("fill", "steelblue")
+        .attr("d", area);
+    */
 });
 
 
 console.log(pop);
 
-const draw = () => {
-
+const draw = (d) => {
+    const points = graph.selectAll('circle');
+    points.append('circle')
+        .attr('r', 1)
+        .attr('cx', year => yearScale(year))
 }
